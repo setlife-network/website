5dfa1f2b8d60f2cbe63b1b79cadeb8eae6ef3094
sofiarm21 <49292858+sofiarm21@users.noreply.github.com>
2019-07-04 23:58:18 -0400

    basic UI mockup  of contact form
    closes #6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,96 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Row, Col } from 'antd';
+import { Typography } from 'antd';
+import { Layout } from 'antd';
+import { AutoComplete } from 'antd';
+import { Button, Radio, Icon } from 'antd';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+
+const { Header, Footer, Sider, Content } = Layout;
+const { Title } = Typography;
+const { Text } = Typography;
+
+class roundButton extends Component {
+    state = {
+        size: 'large',
+    };
+
+
+    handleSizeChange = e => {
+        this.setState({ size: e.target.value });
+    };
+
+    render(){
+        const { size } = this.state;
+        return(
+            <Button  type="primary" shape="round" class="ant-btn ant-btn-primary" >
+                Download
+            </Button>
+        )
+    }
+ }
+
+
+
+
+class SimpleHolder extends Component{
+    render(){
+        return(
+            <div>
+                <Row clasName = "simpleHolder" >
+                    <Form/>
+
+                </Row>
+            </div>
+
+        )
+    }
+}
+
+class Form extends Component{
+
+    render(){
+        return(
+            <div>
+            <Row className = "head" id = "form">
+                <Col span={8}>
+                    <Title level={2}>Stay tuned!</Title>
+                    <Text >Suscribe to be up to date of our new courses</Text>
+                    <roundButton/>
+                </Col>
+            </Row>
+            <Row className = "body" id = "form">
+                <Col span={24}>
+                    <AutoComplete
+                    style={{ width: 200 }}
+                    placeholder="Email"
+                    />
+
+                </Col>
+            </Row>
+            </div>
+        )
+    }
+
+}
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+            <Layout>
+                <Header>Header</Header>
+                <Content>
+                    <SimpleHolder/>
+
+                </Content>
+                <Footer>Footer</Footer>
+            </Layout>
+
+      </div>
+    );
+  }
 }
 
 export default App;
