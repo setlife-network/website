05599fa79c043d7c6274f16bf16fc6a3dc9668eb
Oscar Lafarga <otech47@gmail.com>
2019-09-20 13:14:22 +0200

    Add responsiveness improvements for iPad
    Chrome DevTools is notoriously inaccurate for
    device testing so this should fix some of the
    problems I saw on a real iPad Pro vs the
    iPad Pro rendering in the Dev Tools which
    seemedd fine
    
    Implemented responsiveness font sizes with Bootstrap
    for a potentially more optimized way of sizing
    text

--- src/styles/sizes.js
+++ src/styles/sizes.js
@@ -2,9 +2,9 @@ export default {
     quote: '44px', //'3vw', //44px
     link: '13px', //'1vw', //13px
     button: '17px', //'1.11vw', //17px
-    small: '16px', //'1vw', //16px
-    regular: '22px', //'2vw', //22px
-    medium: '28px', //'3vw', //28px
-    large: '54px', //'4vw', 54px
-    xlarge: '60px' //'5vw' 64px
+    small: '1rem', //'1vw', //16px
+    regular: '1.375rem', //'2vw', //22px
+    medium: '1.75rem', //'3vw', //28px
+    large: '3.375rem', //'4vw', 54px
+    xlarge: '3.75' //'5vw' 64px
 }
