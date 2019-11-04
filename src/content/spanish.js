28f9db6b46802f0d8e1ded3ff66162f307830db3
viki <blankwaving@gmail.com>
2019-11-04 10:21:54 +0100

    Replace English text with Spanish translation

--- src/content/spanish.js
+++ src/content/spanish.js
@@ -8,22 +8,22 @@ const BLOGBUTTON = [
 
 const BLOGHEADLINE = [
     {
-        headline: 'About Us',
+        headline: 'Sobre nosotros',
         key: 'key-blogHeadline'
     }
 ]
 
 const COOKIESCONSENT = [
     {
-        description: 'Esta pagina usa cookies',
-        agreement: 'Acepto'
+        description: 'Ésta página web utiliza cookies para que tu experiencia sea la mejor posible.',
+        agreement: 'Sí, accepto.'
     }
 ]
 
 const CONSULTATIONBUTTON = [
     {
         url: 'https://techprep.org/',
-        description: 'Learn more',
+        description: 'Más información',
         key: 'key-consultationButton'
     }
 
@@ -31,8 +31,8 @@ const CONSULTATIONBUTTON = [
 
 const CONSULTATIONHEADLINE = [
     {
-        headline: 'Tech Careers',
-        description: 'TechPrep by Facebook is one of the many resources available to start exploring how technology can help upgrade your career',
+        headline: 'Trabajar en tecn',
+        description: 'TechPrep by Facebook es uno de los numerosos recursos a tu disposición para descrubrir cómo la tecnología puede mejorar tu trayecto professional',
         key: 'key-consultationHeadline'
     }
 
@@ -40,37 +40,37 @@ const CONSULTATIONHEADLINE = [
 
 const COREVALUES = [
     {
-        headline: 'Intellectual Freedom',
-        description: 'It is the right of every individual to both seek and receive information from all points of view without restriction',
+        headline: 'Libertad intelectual',
+        description: 'Todos y cada uno de nosotros tenemos el derecho tanto de buscar como de encontrar información desde toda perspectiva sin restricción alguna',
         key: 'key-value1'
     },
     {
-        headline: 'Learn by Teaching',
-        description: 'The deepest understanding of a topic comes when you can teach it to anyone willing to learn',
+        headline: 'Aprender enseñando',
+        description: 'La comprensión mas profunda de un concepto viene con la habilidad de enseñarle a quien sea que tenga interés en aprender',
         key: 'key-value2'
     },
     {
-        headline: 'Open-source software "libre"',
-        description: 'To promote true "freedom", users must be able to run software that can be studied, changed, adapted, and distributed in any variation',
+        headline: 'Código abierto y "libre"',
+        description: 'Para que se fomente una "libertad" verdadera, los usuarios tienen que poder ejecutar programas que se pueden estudiar, cambiar, adaptar y distribuir de cualquier manera',
         key: 'key-value3'
     },
     {
-        headline: 'Sustainability',
-        description: 'Rapid technology growth may have dangerous socio-economic effects and should be tempered with secure & sustainable practices in both business & engineering',
+        headline: 'Sostenbilidad',
+        description: 'Dejar que la tecnología avance con demasiada rapidez podría perjudicarnos a nivel social y económico. Se tiene que usar métodos de trabajo seguros y sostenibles tanto a nivel comerical como de ingeniería',
         key: 'key-value4'
     },
 ]
 
 const COREVALUESHEADLINE = [
     {
-        headline: 'Core Values',
+        headline: 'Valores Clave',
         key: 'key-valuesHeadline'
     }
 ]
 
 const HEADERBUTTON = [
     {
-        description: 'Get a free consultation',
+        description: 'Consigue tu consulta gratuita',
         url: 'url',
         key: 'key-button'
     }
@@ -82,72 +82,72 @@ const HEADERHEADLINE = {
 
 
 const HEADERDROPDOWN = {
-    english: 'inglés',
+    english: 'english',
     spanish: 'español'
 }
 
+
 const HEADERLINKS = [
     {
-        text: 'Mission',
-        href: '#home',
-        key: 'key-Mission'
-    },
-    {
-        text: 'Curriculum',
-        href: '#home',
+        text: 'Plan de estudio',
+        href: 'https://tech-education.teachable.com',
         key: 'key-Curriculum'
     },
     {
-        text: 'Programs',
-        href: '#home',
-        key: 'key-Programs'
+        text: 'Misión',
+        href: 'https://info.setlife.network',
+        key: 'key-Mission'
     },
+    // {
+    //     text: 'Programs',
+    //     href: '#home',
+    //     key: 'key-Programs'
+    // },
 ]
 
 const NEWSLETTER = [
     {
-        title: 'Interested in SetLife?',
-        headline: 'Sign up for our mailing list to receive monthly updates about our programs',
-        description: 'Well never share your email or bother you with spam',
-        pastNewsletters: 'View all past newsletters',
-        placeholder: 'Enter your email',
-        buttonText: 'Signup'
+        title: 'Te interesa lo que hacemos en SetLife?',
+        headline: 'Suscríbete a nuestra lista de correo y cada mes recibirás novedades sobre nuestros programas',
+        description: 'No compartiremos nunca tu dirección de correo electrónico ni te mandaremos spam',
+        pastNewsletters: 'Ver newsletters antiguos',
+        placeholder: 'Escribe aquí tu dirección de correo',
+        buttonText: 'Suscribir'
     }
 ]
 
 const PRINCIPALMESSAGE = [
     {
-        headline1: 'Crea Software',
-        headline2: 'Tu superpoder',
-        description: 'Learn valuable technical skills to keep up with technology or even upgrade your career',
-        buttonText: 'Start learning',
-        url: 'url',
+        headline1: 'Haz que el software sea',
+        headline2: 'tu superpoder',
+        description: 'Aprende habilidades tecnológicas tanto para elevar tu carrera profesional como para estar al tanto de los avances en la tecnología',
+        buttonText: 'Empieza a aprender',
+        url: 'https://tech-education.teachable.com',
         key: 'key-principalMessage'
     }
 ]
 
-
 const PROGRAMSHEADLINE = [
     {
-        headline: 'SetLife Programs',
-        description: 'SetLife is developing a variety of programs to help promote collaborative learning and educational engineering',
+        headline: 'Programas SetLife',
+        description: 'SetLife está diseñando varios programas para fomentar el aprendizaje colaborativo y la ingeniería educacional',
         key: 'key-programsHeadline'
     }
 ]
 
 const PROGRAMSLEFT = [
     {
-        headline: 'International Learning & Development',
-        description: 'Train & refine your technical skills and experience modern international work culture in Barcelona, Spain',
-        buttonText: 'Trial 1 Overview',
-        link: 'https://setlife.network/Project-Based-Learning-1-Barcelona',
+        headline: 'Aprendizaje y desarrollo internacional',
+        description: 'Pon a punto tus habilidades tecnológicas trabajando en el ambiente laboral internacional de hoy en día, en Barcelona',
+        buttonText: 'Resumen de la Prueba 1',
+        link: 'https://docs.setlife.network/Project-Based-Learning-1-Barcelona',
         image: 'https://user-images.githubusercontent.com/4914611/63650620-c51d8980-c74c-11e9-89ff-efca941a7e2e.png',
         key: 'key-program1'
     },
     {
-        headline: 'Online Education Courses',
-        description: 'Check out the materials in our core curriculum to learn what technologies are out there for you to explore',
-        buttonText: 'Core Curriculum',
+        headline: 'Cursos educación online',
+        description: 'Échale un vistazo a los materiales de nuestro plan principal de estudio para ver qué tencologías te quedan por descrubrir',
+        buttonText: 'Plan principal de estudios',
         link: 'https://tech-education.teachable.com/',
         image: 'https://user-images.githubusercontent.com/4914611/63650619-c51d8980-c74c-11e9-9303-e36e8c7547cb.png',
         key: 'key-program2'
@@ -156,17 +156,17 @@ const PROGRAMSLEFT = [
 
 const PROGRAMSRIGHT = [
     {
-        headline: 'Employee Technical Training',
-        description: 'Invest in your own employees and provide them with the latest tools to contribute to company growth',
-        buttonText: 'Free Consultation',
+        headline: 'Formación técnica para empleados',
+        description: 'Invierte en tus empleados proveéndoles de las últimas herramientas y verás cómo crece tu compañía',
+        buttonText: 'Consulta gratis',
         link: 'https://app.acuityscheduling.com/schedule.php?owner=17420730&appointmentType=11115327',
         image: 'https://user-images.githubusercontent.com/4914611/63650621-c5b62000-c74c-11e9-8741-52a442eeecc0.png',
         key: 'key-program3'
     },
     {
-        headline: 'Mentorship',
-        description: 'Join our mission and help us teach how to teach and learn how to learn',
-        buttonText: 'Join Slack Channel',
+        headline: 'Programa de mentores',
+        description: 'Únete a nuestra misión y nos ayudarás enseñar cómo enseñar y aprender cómo aprender',
+        buttonText: 'Únete al canal de Slack',
         link: 'https://join.slack.com/t/setlife/shared_invite/enQtNzI2MTA2ODQwMTMxLTU4ZDg5YzI5YzI0MGI5ODQwYTM4MGM3NDBkMmJhNWY1NzZlNjJjZGVlOTgxYTIyOGRhNWVjYzM4MDUyNzM5NDM',
         image: 'https://user-images.githubusercontent.com/4914611/63650573-31e45400-c74c-11e9-96ea-9a34d4292c1a.png',
         key: 'key-program4'
@@ -175,32 +175,32 @@ const PROGRAMSRIGHT = [
 
 const QUOTE = [
     {
-        headline: 'If you can’t explain something in simple terms, you don’t understand it',
+        headline: 'Si no lo puedes explicar en términos sencillos, no lo entiendes lo suficiente',
         author: 'Richard Feynman',
-        company: 'Theoretical Physicist',
+        company: 'Físico teórico',
         key: 'key-quote'
     }
 ]
 
 const TEASERS = [
     {
-        headline: 'Fundamentos tecnologicos',
-        description: 'We’ll teach you the core concepts you need to understand technology and anticipate the impact of its exponential growth on your daily life',
-        linkTitle: 'Core curriculum',
+        headline: 'Los fundamentos de la tecnología',
+        description: 'Te enseñaremos conceptos clave para que puedas entender la tecnólogía, observar cómo está creciendo de forma exponencial y prever el impacto que tendrá en tu día a día',
+        linkTitle: 'Plan básico de estudio',
         url: 'https://tech-education.teachable.com/',
         key: 'key-techFundamentals'
     },
     {
-        headline: 'Aprendizaje Project-based',
-        description: 'We’ll help you apply new knowledge directly to existing community projects (or help you start your own!) so that you can train your skills and gain valuable work experience',
-        linkTitle: 'Learn more',
+        headline: 'Aprender a través de proyectos',
+        description: 'Te ayudaremos usar tu conocimientos recién adquiridos para involucrarte en proyectos en común (¡o a inciar tu propio proyecto!) para que puedas afinar tus habilidades y ganar experiencia laboral muy valiosa',
+        linkTitle: 'Mas información',
         url: 'https://medium.com/the-setlife-network/an-introduction-to-project-based-learning-65b4ab70bec2',
         key: 'key-projectBasedLearning'
     },
     {
-        headline: 'Desarrollo Open-source',
-        description: 'With enough experience, you can use your skills to contribute to any open-source project you may be passionate about',
-        linkTitle: 'Explore projects',
+        headline: 'Desarrollo de código libre',
+        description: 'Con el tiempo y la experiencia podrás contribuir a cualquier proyecto de código abierto que te apasione',
+        linkTitle: 'Descrube proyectos',
         url: 'https://github.com/setlife-network',
         key: 'key-OpenSourceDev'
     }
@@ -210,38 +210,45 @@ const TEASERS = [
 const TESTIMONIALS = [
     {
         name: 'Oscar Lafarga',
-        quote: 'SetLife has become my long-term commitment to a sustainable future when it comes to technology and its impact on society. In a world where information & communication moves at the speed of light, constraining our education to the classroom leaves us woefully under-equipped to deal with the major global problems we face. My goal is to incentivize teaching as one of the highest-paid and most valued professions, toppling the knowledge-hoarding power structures that sustain a global inequality of wealth',
-        date: 'President & CTO',
+        quote: 'SetLife representa para mí un compromiso a largo plazo hacia un futuro mas sostenible en cuanto a la tecnología y el impacto que tiene en la sociedad. Hoy en día, cuando la información y la comunicación se mueven a la velocidad de la luz, limitar el aprendizaje a el aula nos impedirá a la hora de enfrentar los problemas globales que están surgiendo. Mi objectivo es incentivar la enseñanza y situarla entre las profesiones mas valoradas y bien remuneradas y así desmantelar las estructuras que han permitido que se acumulara el poder en las manos de pocos y que perpetúan la desigualdad de riqueza a nivel global',
+        date: 'Presidente y CTO',
         portrait: 'https://user-images.githubusercontent.com/4914611/63651576-6e1cb200-c756-11e9-88f2-87cb80faa5a1.png'
     },
     {
         name: 'Viki Lafarga',
-        quote: 'The SetLife Network’s project-based learning trial was a unique opportunity for me in that I was both learning about and gaining work experience in several areas at once: corporate governance, programming and design, productivity, team management, and more. I am particularly interested in how the different areas that make up an organization interact together and in working at those intersections, so I was grateful for a work environment where I had to sync up with my team every week and learn about what else was going on . And not just what my coworkers were doing but also what obstacles they were coming up against, which I think makes for more solid teamwork. That’s really not something most people get to experience in a society where work is mostly specialized into separate departments.',
-        date: 'Dir. of International Operations',
+        quote: 'La prueba que hicimos SetLife con el aprendizaje basado en proyectos para mí fue una oportunidad única porque a la vez que aprendía estaba ganando experiencia laboral en muchos campos a la vez, como la gobernanza empresarial, la programación y el diseño, la productividad y la gestión de equipos entre otros. Me interesa especialmente cómo todos los aspectos de una organización interactúan entre sí y en trabajar en todas esas interfaces, entonces me sentía muy agradecida de trabajar en un ambiente en el que todos nosotros como equipo nos reuníamos todas las semanas y aprendíamos lo que estaban haciendo lo demás. Y no solo lo que hacían sino los obstáculos que encontraban en el camino, cosa que creo que nos fortalece como equipo. Realmente no es algo que muchas personas puedan experimentar en una sociedad donde el trabajo se divide tan marcadamente en departamentos especializados.',
+        date: 'Dir. de operaciones internacionales',
         portrait: 'https://media.licdn.com/dms/image/C4D03AQHFpkfc8bQt5g/profile-displayphoto-shrink_800_800/0?e=1572480000&v=beta&t=-J7whAKcGixu0JyYzmVFXJgxPyD667cw5qNq580GmMA'
     },
     {
         name: 'Quinn Pruitt',
-        quote: 'Project based learning is by far the most effective learning environment for software development. In seven weeks, I learned more about backend development and product management than I would have learned in an entire semester in college. The projects were quite challenging, yet incredibly rewarding. The high level of autonomy and personal responsibility provided a strong sense of ownership for my work, which I found crucial to my growth and success. I would recommend this program to anyone looking to level up their professional skills in a fun an engaging learning environment.',
-        date: 'Dir. of Tech, UX Engineer',
+        quote: 'El aprendizaje basado en proyectos es en definitivo la forma mas efectiva para aprender a programar. En 7 semanas he aprendido mas sobre el desarrollo backend y la gestión de producto que en un semestre universitario. Los proyectos me retaron pero terminarlos era muy gratificante. Tenía un nivel de autonomía y responsabilidad personal que me daba una sensación importante de autoría sobre mi trabajo y que me ayudo a crecer y a conseguir mis objetivos. Recomendaría este programa a quien busque elevar sus habilidades profesionales en un ambiente de aprendizaje divertido y interesante.',
+        date: 'Asesor técnico',
         portrait: 'https://media.licdn.com/dms/image/C5603AQEtPbVdzw90IQ/profile-displayphoto-shrink_800_800/0?e=1572480000&v=beta&t=FQ4lOzdfOXfou0PvkotYPpP59hdqDXFEJsntss3b7q4'
+    },
+    {
+        name: 'David Lafarga',
+        quote: '',
+        date: 'Fundador y colaborador',
+        portrait: 'https://user-images.githubusercontent.com/4914611/65235439-e61c9480-dad6-11e9-9b61-8f705cdd7050.png',
+        // portrait: 'https://user-images.githubusercontent.com/4914611/65234991-01d36b00-dad6-11e9-949e-a1e0709919fa.png'
     },
     {
         name: 'Sofia Rodriguez',
         quote: '',
-        date: 'Software Developer',
+        date: 'Desarrolladora de software',
         portrait: 'https://user-images.githubusercontent.com/4914611/64039373-c0950e80-cb5a-11e9-96db-3c4e553f8107.png'
     },
     {
         name: 'Rebeca Rubio',
         quote: '',
-        date: 'Software Developer',
+        date: 'Desarrolladora de software',
         portrait: 'https://media.licdn.com/dms/image/C4D03AQFoT9fQvjPGpQ/profile-displayphoto-shrink_200_200/0?e=1572480000&v=beta&t=p-NNuPmF7ryNzLONuZ9YE28YzXTouXJa4A0mP68AVJ8'
     },
     {
         name: 'Marin Basic',
         quote: '',
-        date: 'Visual Brand Developer',
+        date: 'Diseñador de marca visual',
         portrait: 'https://media.licdn.com/dms/image/C4E03AQGI2DiXXt3ldw/profile-displayphoto-shrink_200_200/0?e=1572480000&v=beta&t=sK8ZEkVTAxUH5_6oGiDX5d2VGWmjAdHIQqWN5kCveEc'
     },
 ]
