7b6755edb335deac06cb9542a584ca4b2dce7310
Sofia Rodriguez <sofiaromorales@gmail.com>
2019-12-01 18:44:37 -0400

    UI: Teaser Tile flexible height

--- src/components/TeaserTile.js
+++ src/components/TeaserTile.js
@@ -10,12 +10,9 @@ import DividerLine from './DividerLine'
 import theme from '../styles/theme';
 import Text from './Text';
 
-const TextWrapper = styled(Col)`
-    height:150px
-`
 
 const TitleWrapper = styled(Col)`
-    height:100px
+
     margin-top: 10px;
 `
 
@@ -49,13 +46,13 @@ const TeaserTile = ({
                     {headline}
                 </Text>
             </TitleWrapper>
-            <TextWrapper md={11}>
+            <Col md={11}>
                 <Text
                     size={theme.sizes.body}
                 >
                     {intro}
                 </Text>
-            </TextWrapper>
+            </Col>
             <Col>
 
                 <LinkButton
